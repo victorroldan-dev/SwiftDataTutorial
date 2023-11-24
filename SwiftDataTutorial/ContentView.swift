24893ed677853cda8024cff4b9e4b926e0bcabf5
Victor Roldan <victorroldan25@gmail.com>
2023-11-24 18:50:28 -0500

    Initial Commit

---
+++ SwiftDataTutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftDataTutorial
+//
+//  Created by Victor Roldan on 24/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftDataTutorial/SwiftDataTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataTutorialApp.swift
+//  SwiftDataTutorial
+//
+//  Created by Victor Roldan on 24/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
