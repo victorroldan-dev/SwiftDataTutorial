26d76daae8705033a408390469508954ee3115ac
Victor Roldan <vitorroldan.dev@gmail.com>
2023-12-02 13:34:29 -0500

    ModelContainers

--- SwiftDataTutorial/SwiftDataTutorialApp.swift
+++ SwiftDataTutorial/SwiftDataTutorialApp.swift
@@ -9,10 +9,48 @@ import SwiftData
 
 @main
 struct SwiftDataTutorialApp: App {
+    let container: ModelContainer = {
+        let container : ModelContainer!
+        
+        let schema = Schema([CountryModel.self])
+        
+        let config = ModelConfiguration("CountryModelSchema",
+                                        schema: schema,
+                                        isStoredInMemoryOnly: false,
+                                        allowsSave: true)
+        
+        do{
+            container = try ModelContainer(for: schema, configurations: config)
+        }catch{
+            fatalError("falló")
+        }
+
+        return container
+    }()
+    /*
+    let container: ModelContainer = {
+        let container : ModelContainer!
+        
+        let config = ModelConfiguration(url: URL.downloadsDirectory.appending(path: "CountryModel.store"))
+        
+        do{
+            container = try ModelContainer(for: CountryModel.self, configurations: config)
+        }catch{
+            fatalError("falló")
+        }
+
+        return container
+    }()
+    */
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(for: [CountryModel.self])
+        .modelContainer(container)
+    }
+    
+    init(){
+        print(URL.applicationSupportDirectory.path(percentEncoded: false))
     }
 }
